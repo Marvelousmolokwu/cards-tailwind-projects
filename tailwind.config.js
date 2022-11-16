d697789302f877696b765d4bd19296229cb714c0
Marvelousmolokwu <qmolokwu@gmail.com>
2022-11-16 12:57:08 +0100

    changes made to make site more responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        df: "280px",
+        sm: "375px",
+        md: "768px",
+        lg: "910px",
+        xl: "1024px",
+        xxl: "1400px",
+      },
+      fontFamily: {
+        body: ["Poppins"],
+      },
+    },
   },
   plugins: [],
-}
+};
